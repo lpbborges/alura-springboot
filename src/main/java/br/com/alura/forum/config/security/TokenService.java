0abc379831c8fa2273831ccec2761c8ea70f104a
unknown <LuizBorges@RASENGAN.icarotech.com>
2021-10-08 18:02:21 -0300

    feat: add jwt auth

---
+++ src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -0,0 +1,34 @@
+package br.com.alura.forum.config.security;
+
+import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${forum.jwt.expiration}")
+    private String expiration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String gerarToken(Authentication authentication) {
+        Usuario usuario = (Usuario) authentication.getPrincipal();
+        Date now = new Date();
+        Date dataExpiracao = new Date(now.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("API do Fórum da Alura")
+                .setSubject(usuario.getId().toString())
+                .setIssuedAt(now)
+                .setExpiration(dataExpiracao)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
